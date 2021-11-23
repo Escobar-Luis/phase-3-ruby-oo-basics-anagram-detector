7981bbdac1e6109420fb23a93c659c41ae8661ac
Luis Escobar <lae311@stern.nyu.edu>
2021-11-23 17:20:51 -0500

    commit tree 1443ad768be148fca549ebed1d6ac202b0e8d751

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+    # why is this an accessor eventhough it is being intialized
+    attr_accessor :word
+
+    def initialize(word)
+        @word= word
+    end
+
+    def match(array)
+        array.select {|x| x.split("").sort == @word.split("").sort}
+    end
+
+end
